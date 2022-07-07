29cd75cafb834bb6f9c35f871c45311814015ca2
Heagan Henry <76776249+heaganhenry@users.noreply.github.com>
2022-07-07 11:59:08 -0400

    Complete ReverseWords

---
+++ src/main/scala/com/rockthejvm/strings/ReverseWords.scala
@@ -0,0 +1,14 @@
+package com.rockthejvm.strings
+
+object ReverseWords {
+  // Given a string return a result string with the order of each word reversed.
+  // You are also expected to remove any additional blank spaces, e.g. "  hello  world  " -> "world hello"
+  def reverseWords(string: String): String = {
+    string.split(" ").filterNot(_.isEmpty).reverse.mkString(" ")
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(reverseWords("Alice loves Scala")) // "Scala loves Alive"
+    println(reverseWords("    hello    world    ")) // "world hello"
+  }
+}
