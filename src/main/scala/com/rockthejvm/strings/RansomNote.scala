401c3bbe915d418b9b4eb59da5cb9a3333251cc8
Heagan Henry <76776249+heaganhenry@users.noreply.github.com>
2022-07-08 07:50:59 -0400

    Refactor RansomNote

--- src/main/scala/com/rockthejvm/strings/RansomNote.scala
+++ src/main/scala/com/rockthejvm/strings/RansomNote.scala
@@ -3,8 +3,7 @@ package com.rockthejvm.strings
 object RansomNote {
   // Given two strings, note and magazine, write a function that confirms if enough characters are available in the magazine to compose the note
   def ransomNote(note: String, magazine: String): Boolean = {
-    def buildMap(string: String) =
-      string.groupBy(identity).view.mapValues(_.length).toMap
+    def buildMap(string: String) = string.groupBy(identity).view.mapValues(_.length).toMap
 
     val noteMap = buildMap(note)
     val magazineMap = buildMap(magazine)
