44153f79edc8ab10b7e456ff6c43ccf594b70df1
Heagan Henry <76776249+heaganhenry@users.noreply.github.com>
2022-07-13 07:01:00 -0400

    Complete GraphDegrees

--- src/main/scala/com/rockthejvm/graphs/GraphProblems.scala
+++ src/main/scala/com/rockthejvm/graphs/GraphProblems.scala
@@ -14,7 +14,23 @@ object GraphProblems {
     "Mary" -> Set("Bob", "Charlie")
   )
 
-  def main(args: Array[String]): Unit = {
+  /**
+   * Easy problems
+   */
+  // number of nodes this `node` is associated (adjacent) to
+  def outDegree[T](graph: Graph[T], node: T): Int =
+    graph.get(node).map(_.size).getOrElse(0)
 
+  // number of nodes connected no `node`
+  def inDegree[T](graph: Graph[T], node: T): Int =
+    graph.count(_._2.contains(node))
+
+  def main(args: Array[String]): Unit = {
+    def testEasyProblems() = {
+      println(outDegree(socialNetwork, "Alice")) // 3
+      println(inDegree(socialNetwork, "David")) // 2
+    }
+
+    testEasyProblems()
   }
 }
