2ad77127ae9b011842b8ac3fa45c6e119f87b387
Heagan Henry <76776249+heaganhenry@users.noreply.github.com>
2022-07-13 07:03:08 -0400

    Complete PathCheck

--- src/main/scala/com/rockthejvm/graphs/GraphProblems.scala
+++ src/main/scala/com/rockthejvm/graphs/GraphProblems.scala
@@ -25,12 +25,46 @@ object GraphProblems {
   def inDegree[T](graph: Graph[T], node: T): Int =
     graph.count(_._2.contains(node))
 
+  /**
+   * Medium problems
+   */
+  // confirm if a path exists in a graph between two nodes
+  def isPath[T](graph: Graph[T], start: T, end: T): Boolean = {
+    /*
+    Alice -> Mary
+    = ipt([Alice], [])
+    = ipt([Bob, Charlie, David], [Alice])
+    = ipt([Charlie, David], [Bob, Alice])
+    = ipt([David], [Charlie, Bob, Alice])
+    = ipt([Mary], [David, Charlie, Bob, Alice])
+    = true
+    */
+    @tailrec
+    def searchTailrec(nodesToCheck: Set[T], nodesChecked: Set[T]): Boolean = {
+      if (nodesToCheck.isEmpty) false
+      else {
+        val node = nodesToCheck.head
+        val connections = graph(node)
+        if (connections.contains(end)) true
+        else searchTailrec(nodesToCheck - node ++ connections -- nodesChecked, nodesChecked + node)
+      }
+    }
+
+    if (!graph.contains(start) || !graph.contains(end)) false
+    else searchTailrec(Set(start), Set())
+  }
+
   def main(args: Array[String]): Unit = {
     def testEasyProblems() = {
       println(outDegree(socialNetwork, "Alice")) // 3
       println(inDegree(socialNetwork, "David")) // 2
     }
 
-    testEasyProblems()
+    def testMediumProblems() = {
+    println(isPath(socialNetwork, "Alice", "Mary")) // true
+    println(isPath(socialNetwork, "Bob", "Charlie")) // false
+    }
+
+    testMediumProblems()
   }
 }
